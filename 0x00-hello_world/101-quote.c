e2c10328b43483252939d9a0a209c43cc1a34a90
AbduHany <abdu.hany@gmail.com>
2023-10-05 19:19:26 +0000

    Add print to STDERR C program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints message on stderr stream.
+ *
+ * Return: returns 1 if message was sent to stderr.
+ */
+int main(void)
+{
+	fprintf(stderr,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
