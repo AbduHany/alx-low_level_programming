fbdda3a9d9e5dd73e3eff5e94e5fffeb9680b381
AbduHany <abdu.hany@gmail.com>
2023-10-05 18:23:25 +0000

    Modify 4-puts C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - main function prints literal string using puts.
+ *
+ * Return: returns 0 if successful
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
