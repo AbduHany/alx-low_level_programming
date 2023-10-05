e2245da9af8f976c0d2555cb62e0f101a07f129d
AbduHany <abdu.hany@gmail.com>
2023-10-05 18:33:07 +0000

    Add C program that prints string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints string using printf function.
+ *
+ * Return: returns 0 if successful.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
