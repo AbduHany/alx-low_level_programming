02d2c2cfbad215f2818183936d3cef83c2870285
AbduHany <abdu.hany@gmail.com>
2023-10-06 17:26:25 +0300

    Add print alphabet C program

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function prints the alphabet in lowercase.
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char a;
+
+	a = 'a';
+	while (a < 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
