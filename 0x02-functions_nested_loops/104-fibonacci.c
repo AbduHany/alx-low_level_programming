4795178b346941d29d7f56d155d7ea17b9470c19
AbduHany <abdu.hany@gmail.com>
2023-10-11 02:34:26 +0300

    Modify 104-fibonacci.c program

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,17 +8,20 @@
  */
 int main(void)
 {
-	unsigned long int i, n1, n2, n3;
+	unsigned __int128 n1
+	unsigned __int128 n2;
+	unsigned __int128 n3;
+	int i;
 
 	n1 = 0;
 	n2 = 1;
 	for (i = 1; i < 98; i++)
 	{
 		n3 = n1 + n2;
-		printf("%lu, ", n3);
+		printf("%llu, ", n3);
 		n1 = n2;
 		n2 = n3;
 	}
-	printf("%lu\n", n1 + n2);
+	printf("%llu\n", n1 + n2);
 	return (0);
 }
