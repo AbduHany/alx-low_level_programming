0178fb7df65ec86cdfb901779f93c689cce537f1
AbduHany <abdu.hany@gmail.com>
2023-10-16 21:39:05 +0300

    Modify 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,19 @@ void puts_half(char *str)
 	int i, length;
 
 	length = strlen(str);
-	for (i = (length / 2); i < length; i++)
+	if ((length % 2) == 0)
 	{
-		_putchar(*(str + i));
+		for (i = length / 2; i < length; i++)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	else
+	{
+		for (i = ((length / 2) + 1); i < length; i++)
+		{
+			_putchar(*(str + i));
+		}
 	}
 	_putchar('\n');
 }
