9ad4c050d57d980d0f4a9d2257fb9fc048c3f667
AbduHany <abdu.hany@gmail.com>
2023-10-19 00:29:10 +0300

    Add 2-strncpy.c definition

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n bytes of a string.
+ * @dest: pointer to destination string.
+ * @src: pointer to source string.
+ * @n: number of bytes to be copied.
+ *
+ * Return: pointer to copied string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; (dest[i] != '\0' || src[i] != '\0') && (i < n); i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[97] = '\0';
+	return (dest);
+}
