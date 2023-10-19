f22bad0d3b60315f810b4f291b5d79855ad74338
AbduHany <abdu.hany@gmail.com>
2023-10-19 13:25:24 +0300

    Add 7-leet.c definition

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ * @s: pointer to string to be encoded.
+ *
+ * Return: pointer to encoded string.
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char *upper, *lower, *code;
+
+	i = 0;
+	upper = "AEOTL";
+	lower = "aeotl";
+	code= "43071";
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == upper[j] || s[i] == lower[j])
+			{
+				s[i] = code[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+
+}
