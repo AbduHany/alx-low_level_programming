f86079f3488e91d217c521147897a2f85e285e3a
AbduHany <abdu.hany@gmail.com>
2023-10-21 00:51:10 +0300

    Modify 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -33,7 +33,7 @@ void print_buffer(char *b, int size)
 		{
 			if ((i + byte) >= size)
 				break;
-			else if (b[i + byte] >= 33 && b[i + byte] <= 126)
+			else if (b[i + byte] >= 31 && b[i + byte] <= 126)
 			{
 				printf("%c", b[i + byte]);
 			}
