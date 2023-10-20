bfc5685d997760be50562edd0b42a8f649ad8205
AbduHany <abdu.hany@gmail.com>
2023-10-20 20:33:40 +0300

    Modify 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -25,7 +25,6 @@ void rev_str(char *s)
 		s[len - 1 - i] = temp;
 	}
 }
-
 /**
  * infinite_add - adds two numbers.
  * @n1: pointer to first integer string.
@@ -37,51 +36,32 @@ void rev_str(char *s)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, l = 0, len1 = 0;
-	int len2 = 0, len3 = 0, carry = 0, d1 = 0, d2 = 0, sum = 0;
+	int i, j, l, len1, len2, carry, d1, d2, sum;
 
 	for (i = 0; n1[i] != '\0'; i++)
 		len1++;
 	for (i = 0; n2[i] != '\0'; i++)
 		len2++;
-	if (len1 > size_r || len2 > size_r)
+	if (len1 >= size_r || len2 >= size_r)
 		return (0);
-	rev_str(n1);
-	rev_str(n2);
-	i = 0;
-	j = 0;
-	l = 0;
 	carry = 0;
-	while (i < len1 || j < len2)
+	for (i = len1 - 1, j = len2 - 1, l = 0; i >= 0 || j >= 0 || carry == 1;
+			 i--, j--, l++)
 	{
-		d1 = i < len1 ? n1[i] - '0' : 0;
-		d2 = j < len2 ? n2[j] - '0' : 0;
-		if (sum + carry > 9)
-		{
-			carry++;
-			sum = 0;
-		}
+		d1 = i < 0 ? 0 : n1[i] - '0';
+		d2 = j < 0 ? 0 : n2[j] - '0';
 		sum = d1 + d2 + carry;
+		if (sum >= 10)
+			carry = 1;
+		else
+			carry = 0;
+		if (l >= (size_r - 1))
+			return (0);
 		r[l] = (sum % 10) + '0';
-		carry = sum / 10;
-		i++;
-		j++;
-		l++;
 	}
-	if (carry == 1)
-	{
-		r[l] = '1';
-		r[l + 1] = '\0';
-		len3 = l + 1;
-	}
-	else
-	{
-		r[l] = '\0';
-		len3 = l;
-	}
-	if (len3 > size_r)
-	{
+	if (l == size_r)
 		return (0);
-	}
+	r[l] = '\0';
+	rev_str(r);
 	return (r);
 }
