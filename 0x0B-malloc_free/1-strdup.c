2bfa2f7e2639bcf427f3b30a464bdb38d02cf80f
AbduHany <abdu.hany@gmail.com>
2023-10-30 16:54:23 +0200

    Modify 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,8 +16,6 @@ char *_strdup(char *str)
 	len = 0;
 	if (str == NULL)
 		return (NULL);
-	if (*str == '\0')
-		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
 	cpy = malloc((len + 1) * sizeof(char));
