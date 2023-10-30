7830f95ef561b2b242e3ec0f603affdcb8dc6022
AbduHany <abdu.hany@gmail.com>
2023-10-30 19:07:09 +0200

    Modify 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,10 @@ int **alloc_grid(int width, int height)
 		if (p[i] == NULL)
 		{
 			for (k = 0; k < i; k++)
+			{
 				free(p[k]);
+			}
+			free(p);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
