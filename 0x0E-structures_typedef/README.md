731d2152b15ba8f51b762cfbc07416aab969cb95
AbduHany <abdu.hany@gmail.com>
2023-11-07 01:30:44 +0200

    Add dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+# C - Structures, typedef
+
+This directory contains all the solutions & code for the "C - Structures, typedef" project (0x0E-structures_typedef).
+Each file in this directory corresponds to a task in the project.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+/**
+ * struct dog - a struct for dog data types.
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
