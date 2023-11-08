c22880d14afc501098b4e01fe2174a042f024a4c
AbduHany <abdu.hany@gmail.com>
2023-11-08 15:36:22 +0200

    Modify 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,15 +12,13 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	
+
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
-		else
-			continue;
 	}
 	return (-1);
 }
