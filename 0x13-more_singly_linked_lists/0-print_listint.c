eee44c49927d351723bb504a5a5460110f761c5c
AbduHany <abdu.hany@gmail.com>
2023-11-20 10:58:10 +0200

    First Commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: head pointer to the listint_t list.
+ *
+ * Return: the number of nodes in the list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t node_count = 0;
+	listint_t *temp;
+
+	temp = (listint_t *) h;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		node_count++;
+		temp = temp->next;
+	}
+	return (node_count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+
+# C - More singly linked lists
+
+This directory contains all the solutions & code for the "C - More singly linked lists" project (0x13-more_singly_linked_lists).
+Each file in this directory corresponds to a task in the project.
