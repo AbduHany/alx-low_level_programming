00ed7a8d185769b180fc99b6ce86039ab0eb9c81
AbduHany <abdu.hany@gmail.com>
2023-11-21 22:53:12 +0200

    Modify 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
- * check_loop - checks if linked list has a loop or not.
+ * get_loop_start - gets the pointer of where a loop starts in a list.
  * @head: pointer to head of linked list.
  *
- * Return: 0 if list contains no loop, number of unique nodes
- * if list contains loop.
+ * Return: NULL if list contains no loop, pointer of where a loop
+ * starts in a list.
  */
 listint_t *get_loop_start(listint_t *head)
 {
-	const listint_t *fast, *slow;
+	listint_t *fast, *slow;
 	size_t node_count = 1;
 
 	if (head->next == NULL)
@@ -35,17 +35,18 @@ listint_t *get_loop_start(listint_t *head)
 	return (NULL);
 }
 /**
- * print_listint_safe - prints a listint_t linked list.
+ * find_listint_loop - finds the loop in a linked list.
  * @head: pointer to head of listint_t list.
  *
- * Return: number of nodes in list.
+ * Return: address of node where the loop starts or
+ * NULL if there is no loop.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *temp;
 
 	if (head == NULL)
-		exit(98);
+		return (NULL);
 	temp = get_loop_start(head);
 	if (temp == NULL)
 	{
