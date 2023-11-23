1d4f2e6d27239e339777f77650d7e4543ed8927a
AbduHany <abdu.hany@gmail.com>
2023-11-23 23:08:25 +0200

    Modify 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -21,8 +21,8 @@ int get_bit(unsigned long int n, unsigned int index)
 		bitlength++;
 		temp_num = temp_num >> 1;
 	}
-	if (index > (bitlength - 1))
+	if (index >= bitlength)
 		return (-1);
-	bitval = (n >> index) & 1;
+	bitval = ((n >> index) & 1);
 	return (bitval);
 }
