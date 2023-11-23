5a64ed95449649555f1648a61242317048fa1755
AbduHany <abdu.hany@gmail.com>
2023-11-23 22:46:42 +0200

    Modify 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,5 +9,16 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int bitlength = 0;
+	unsigned long int temp_num;
+	
+	temp_num = n;
+	while (temp_num != 0)
+	{
+		bitlength++;
+		temp_num = temp_num >> 1;
+	}
+	if (index >= bitlength || index <= 0)
+		return (-1);
 	return (((n >> index) & 1));
 }
