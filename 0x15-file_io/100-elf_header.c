90c42d4c6248f54da463685e054724ce93c5c88a
AbduHany <abdu.hany@gmail.com>
2023-11-28 04:45:06 +0200

    Modify 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -194,9 +194,9 @@ void printentry(unsigned long int e_entry, unsigned char *e_ident)
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		e_entry = (e_entry >> 24) |
-              ((e_entry >> 8) & 0xff00) |
-              ((e_entry << 8) & 0xff0000) |
-              (e_entry << 24);
+			((e_entry >> 8) & 0xff00) |
+			((e_entry << 8) & 0xff0000) |
+			(e_entry << 24);
 	}
 	if (e_ident[EI_CLASS] == ELFCLASS32)
 		printf("0x%x\n", (unsigned int)e_entry);
