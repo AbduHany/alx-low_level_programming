86310e391b7a078f3cb3b29fdecee5e62d5f280b
AbduHany <abdu.hany@gmail.com>
2024-01-11 16:53:32 +0200

    Modify 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,8 +22,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		*head = new_node;
 		return (new_node);
 	}
-        /** adding it to a list that already has nodes **/
-        new_node->next = *head;
+	/** adding it to a list that already has nodes **/
+	new_node->next = *head;
 	(*head)->prev = new_node;
 	*head = new_node;
 	return (new_node);
