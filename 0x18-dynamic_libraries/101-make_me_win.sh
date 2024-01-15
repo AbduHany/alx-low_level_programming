92fcf7ec328459fff2a4591078e4d451d727600e
AbduHany <abdu.hany@gmail.com>
2024-01-15 17:55:46 +0200

    Modify 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -P /tmp https://github.com/AbduHany/alx-low_level_programming/raw/master/0x18-dynamic_libraries/_printf.so
+wget -P /tmp https://github.com/AbduHany/alx-low_level_programming/raw/master/0x18-dynamic_libraries/_printf.so
 export LD_PRELOAD=/tmp/_printf.so
