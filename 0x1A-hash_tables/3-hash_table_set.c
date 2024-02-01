9a14a4765e4787ed94daf26ea798435e1b7c3b44
AbduHany <abdu.hany@gmail.com>
2024-02-01 21:16:25 +0200

    Modify 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,6 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newentry, *cursor;
-	int keylen, valuelen;
 	unsigned long int newindex;
 	char *nodekey, *nodeval;
 
@@ -19,18 +18,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (strlen(key) == 0)
 		return (0);
-	keylen = strlen(key);
-	valuelen = strlen(value);
 	newentry = malloc(sizeof(hash_node_t));
 	if (newentry == NULL)
 		return (0);
-	nodekey = malloc(sizeof(char) * (keylen + 1));
+	nodekey = malloc(sizeof(char) * (strlen(key) + 1));
 	if (nodekey == NULL)
 	{
 		free(newentry);
 		return (0);
 	}
-	nodeval = malloc(sizeof(char) * (valuelen + 1));
+	nodeval = malloc(sizeof(char) * (strlen(value) + 1));
 	if (nodeval == NULL)
 	{
 		free(newentry);
@@ -44,10 +41,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	newentry->next = NULL;
 	newindex = key_index((const unsigned char *)key, ht->size);
 	if ((*((ht->array) + newindex)) != NULL)
+	{
 		cursor = (*((ht->array) + newindex));
 		newentry->next = cursor;
 		(*((ht->array) + newindex)) = newentry;
-		return (1);
-	*((ht->array) + newindex) = newentry;
+	}
+	else
+		*((ht->array) + newindex) = newentry;
 	return (1);
 }
