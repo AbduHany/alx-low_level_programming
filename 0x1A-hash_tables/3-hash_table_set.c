c0fc5c5ebb9a1aaffa122ac57b02d946f880d333
AbduHany <abdu.hany@gmail.com>
2024-02-01 21:08:17 +0200

    Modify 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,4 @@
-#include "hash_tables.h"
+`#include "hash_tables.h"
 
 /**
  * hash_table_set - adds an element to the hash table.
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int newindex;
 	char *nodekey, *nodeval;
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 	if (strlen(key) == 0)
 		return (0);
