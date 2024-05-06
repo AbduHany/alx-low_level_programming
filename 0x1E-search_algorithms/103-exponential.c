a4c879ffc2914289312128c44db2ff4cd42bf0fe
AbduHany <abdu.hany@gmail.com>
2024-05-07 00:34:37 +0300

    Add 103-exponential.c

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - searches for a value in a sorted array of integers
+ * using the Exponential search algorithm
+ * @array: array on integers being searched.
+ * @size: size of the array being searched.
+ * @value: target value being searched for in array.
+ *
+ * Return: the first index where value is located,
+ * -1 if array is NULL or value isn't found.
+ */
+int exponential_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+}
